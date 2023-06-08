377350ab985c346367e6f1e9f710b59aab50ef36
Rajat Dixit <Rajatdixithbtu@gmail.com>
2023-06-08 16:08:08 +0530

    implement getter setter

---
+++ src/test/java/Main.java
@@ -0,0 +1,17 @@
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
+import java.net.SocketOption;
+
+public class Main {
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+       // a.name= "Rajat"; not possible beacuse it is private
+     //   System.out.print(a.name);not possible beacuse it is private
+
+        //a.setId(100);
+        a.setName("Rajat");
+        System.out.print(a.getName());
+
+
+    }
+}
