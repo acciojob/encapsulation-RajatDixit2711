36e0efbc79168f0eb7054502943f0f67bde5232b
Rajat Dixit <Rajatdixithbtu@gmail.com>
2023-06-10 00:06:05 +0530

    add2

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        RWOnly a = new RWOnly();
+        RWOnly a = new  RWOnly();
        // a.name= "Rajat"; not possible beacuse it is private
      //   System.out.print(a.name);not possible beacuse it is private
 
 
-       a.setName("Rajat");
-       a.getName();
 
+      a.setName( "rajat");
+      a.getName();
 
     }
 }
--- src/test/java/RWOnly.java
+++ src/test/java/RWOnly.java
@@ -3,7 +3,6 @@ public class RWOnly {
     private String name;
 
 
-
     public String getName() {
         return name;
     }
